ca10add7b365a306db2ed3d1fc248356148f2491
Jecinta wanjiru <jecintawanjirug@gmail.com>
2019-08-18 17:37:43 +0300

    add input form and arrays

---
+++ javascript.js
@@ -0,0 +1,31 @@
+var maleNames={
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+
+};
+var femaleNames={
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama",
+};
+var days=document.getElementById("days").value;
+var moths=document.getElementById("months").value;
+var years=document.getElementById("years").value;
+ 
+if (document.getElementById("male").checked){
+    var gender="male"
+}
+else{
+    var gender="female";
+};
+
+
